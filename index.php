88fd9cbc30c7eb679911fd01f4c1b53592bf1242
Zanco1993 <zanconatofabio@gmail.com>
2022-02-25 16:30:15 +0100

    push

---
+++ index.php
@@ -0,0 +1,39 @@
+<!-- Consegna 
+- è definita una classe ‘Movie’
+   => all’interno della classe sono dichiarate delle variabili d’istanza
+   => all’interno della classe è definito un costruttore
+   => all’interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+-->
+
+<?php
+
+class Movie {
+    public $title;
+    public $genre;
+    public $language;
+    public $yearProduction;
+}
+
+$harryPotter = new Movie() {
+
+}
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop-1</title>
+</head>
+<body>
+    
+</body>
+</html>
