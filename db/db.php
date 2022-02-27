01ad26cd6b76e9ed7c5a85715b6ecfeadc28925b
Zanco1993 <zanconatofabio@gmail.com>
2022-02-27 14:40:16 +0100

    aggiunto array di istanze + include db

---
+++ db/db.php
@@ -0,0 +1,85 @@
+<?php
+
+$harryPotter_1 = new Movie(
+    [
+        "image_album" => 'https://m.media-amazon.com/images/I/51EKlNW5H+L._AC_UL320_.jpg',
+        "title" => "Harry Potter e la pietra filosofale",
+        "genre" => "Fantasy",
+        "language" => "ita",
+        "yearProduction" => "2001-01-01",
+        "valutazione" => 4
+    ]
+);
+// '<p>Il film è stato pubblicato' . ' ' . $firstFilm->getPublishMovie("2001-01-01") . ' ' . 'anni fa' . '</p>' . '<br>';
+
+$harryPotter_2 = new Movie(
+    [
+        "image_album" => 'https://m.media-amazon.com/images/I/81sIPSTxj4L._SL1441_.jpg',
+        "title" => "Harry Pottere e la camera dei segreti",
+        "genre" => "Fantasy",
+        "language" => "ita",
+        "yearProduction" => "2002-01-01",
+        "valutazione" => 5
+    ]
+);
+$harryPotter_3 = new Movie(
+    [
+        "image_album" => 'https://m.media-amazon.com/images/I/81vteywKEKL._SY445_.jpg',
+        "title" => "Harry Potter e il prigioniero di Azkaban",
+        "genre" => "Fantasy",
+        "language" => "ita",
+        "yearProduction" => "2004-01-01",
+        "valutazione" => 5
+    ]
+);
+$harryPotter_4 = new Movie(
+    [
+        "image_album" => 'https://m.media-amazon.com/images/I/911QLjZ-wKL._AC_UL320_.jpg',
+        "title" => "Harry Potter e il calice di fuoco",
+        "genre" => "Fantasy",
+        "language" => "ita",
+        "yearProduction" => "2005-01-01",
+        "valutazione" => 5
+    ]
+);
+$harryPotter_5 = new Movie(
+    [
+        "image_album" => 'https://m.media-amazon.com/images/I/811VtPlOfNL._AC_UL320_.jpg',
+        "title" => "Harry Potter e l'Ordine della Fenice",
+        "genre" => "Fantasy",
+        "language" => "ita",
+        "yearProduction" => "2007-01-01",
+        "valutazione" => 5
+    ]
+);
+$harryPotter_6 = new Movie(
+    [
+        "image_album" => 'https://m.media-amazon.com/images/I/8126t2PaIVL._AC_UL320_.jpg',
+        "title" => "Harry Potter e il principe mezzosangue",
+        "genre" => "Fantasy",
+        "language" => "ita",
+        "yearProduction" => "2009-01-01",
+        "valutazione" => 5
+    ]
+);
+$harryPotter_7 = new Movie(
+    [
+        "image_album" => 'https://m.media-amazon.com/images/I/81qTBZk9QAL._AC_UL320_.jpg',
+        "title" => "Harry Potter e i Doni della Morte - Parte 1",
+        "genre" => "Fantasy",
+        "language" => "ita",
+        "yearProduction" => "2010-01-01",
+        "valutazione" => 5
+    ]
+);
+$harryPotter_8 = new Movie(
+    [
+        "image_album" => 'https://m.media-amazon.com/images/I/81sfECfPBIL._AC_UL320_.jpg',
+        "title" => "Harry Potter e i Doni della Morte - Parte 2",
+        "genre" => "Fantasy",
+        "language" => "ita",
+        "yearProduction" => "2011-01-01",
+        "valutazione" => 5
+    ]
+);
+
--- index.php
+++ index.php
@@ -1,73 +1,97 @@
-<!-- Consegna 
-- è definita una classe ‘Movie’
-   => all’interno della classe sono dichiarate delle variabili d’istanza
-   => all’interno della classe è definito un costruttore
-   => all’interno della classe è definito almeno un metodo
-- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
--->
-
 <?php
+// Consegna 
+// - è definita una classe ‘Movie’
+//    => all’interno della classe sono dichiarate delle variabili d’istanza
+//    => all’interno della classe è definito un costruttore
+//    => all’interno della classe è definito almeno un metodo
+// - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
 
-class Movie {
+
+class Movie
+{
+    public $image_album;
     public $title;
     public $genre;
     public $language;
     public $yearProduction;
+    public $valutazione;
 
-    function __construct($_dataMovies)
+    function __construct(array $_dataMovies)
     {
-        
-     echo $this ->title = $_dataMovies['title'] . '<br>';  
-     echo $this -> genre = $_dataMovies['genre'] . '<br>';
-     echo $this -> language = $_dataMovies['language'] . '<br>';
-     echo $this -> yearProduction = $_dataMovies['yearProduction'] . '<br>'; 
-     
+        $this->image_album = $_dataMovies["image_album"];
+        $this->title = $_dataMovies['title'];
+        $this->genre = $_dataMovies['genre'];
+        $this->language = $_dataMovies['language'];
+        $this->yearProduction = $_dataMovies['yearProduction'];
+        $this->valutazione = $_dataMovies["valutazione"];
     }
 
     // questa funzione permette di calcolare la differenza di anni tra la data produzione e la data odierna
-    public function getPublishMovie($yearPublish) {
-
+    public function getPublishMovie($yearPublish)
+    {
         $yearDifferent = date_diff(date_create($yearPublish), date_create());
-
-        return $yearDifferent -> y;
+        return $yearDifferent->y;
     }
-} 
 
-$firstFilm = new Movie([
-    "title" => "Harry Potter",
-    "genre" => "Fantasy",
-    "language" => "ita", 
-    "yearProduction" => "2001-01-01"
-]);
-    echo '<p>Il film è stato pubblicato' . ' ' . $firstFilm -> getPublishMovie("2001-01-01") . ' ' . 'anni fa' . '</p>' . '<br>';
+    // public function printMovie() {
+        
+    // }
+}
 
-$secondFilm = new Movie([
-    "title" => "Io sono leggenda",
-    "genre" => "Azione",
-    "language" => "ita", 
-    "yearProduction" => "2007-01-01"
-]);
-    echo '<p>Il film è stato pubblicato' . ' ' . $secondFilm -> getPublishMovie("2007-01-01") . ' ' . 'anni fa' . '</p>' . '<br>';
+// importo tutte le instanze 
+include __DIR__ . '/db/db.php';
 
-    
+// '<p>Il film è stato pubblicato' . ' ' . $secondFilm->getPublishMovie("2007-01-01") . ' ' . 'anni fa' . '</p>' . '<br>';
 
+// creo un array delle istanze dei film cosi da ciclarli nell'array
 
+$HarryPotterMovies = [
+    $harryPotter_1,
+    $harryPotter_2,
+    $harryPotter_3,
+    $harryPotter_4,
+    $harryPotter_5,
+    $harryPotter_6,
+    $harryPotter_7,
+    $harryPotter_8
+];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-oop-1</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
+    <div class="container">
+        <h1 class="title">Lista film</h1>
+        <div class="content-card">
 
-<?php
+            <?php foreach ($HarryPotterMovies as $key => $value) {
+                // $value -> printMovie();
+                echo '<div class="card">';
+                foreach ($value as $keyMovie => $valueMovie) {
+                    if ($keyMovie === "image_album") {
+                        echo '<img src="' . $valueMovie . '" alt="">';
+                    } else {
+                        echo '<p><strong>' . ucfirst($keyMovie) . '</strong>' . ':' . $valueMovie;
+                    }
+                }
+                echo '</div>';
+            } ?>
+        </div>
+    </div>
 
 
-?>
-    
+    </div>
+
+
 </body>
+
 </html>
